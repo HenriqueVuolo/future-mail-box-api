4b3cbc6b1ef24b793227460e3bb21849768898d1
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-01-15 18:51:24 -0300

    feat: add findByEmail to UsersRepository

--- src/domain/repositories/users.repository.ts
+++ src/domain/repositories/users.repository.ts
@@ -2,4 +2,5 @@ import {User} from '@domain/entities/users.entity';
 
 export abstract class UsersRepository {
   abstract create: (data: User) => Promise<void>;
+  abstract findByEmail: (email: User['email']) => Promise<User | null>;
 }
