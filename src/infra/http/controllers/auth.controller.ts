94421421339fa5b09dae2eb7181440f3f403c646
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-01-15 18:54:52 -0300

    feat: add login endpoint

--- src/infra/http/controllers/auth.controller.ts
+++ src/infra/http/controllers/auth.controller.ts
@@ -1,12 +1,31 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {User} from '@domain/entities/users.entity';
+import {LocalAuthGuard} from '@infra/auth/guards/local-auth.guard';
+import {Body, Controller, Post, Req, UseGuards} from '@nestjs/common';
+import {ApiOperation, ApiTags} from '@nestjs/swagger';
 import {CreateUser} from '@useCases/create-user';
+import {GenerateToken} from '@useCases/generate-token';
 import {SignUpRequest} from '../dtos/sign-up-request';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
-  constructor(private createUser: CreateUser) {}
+  constructor(
+    private createUser: CreateUser,
+    private generateToken: GenerateToken,
+  ) {}
+
   @Post('signup')
+  @ApiOperation({description: 'Signup'})
   async signup(@Body() payload: SignUpRequest): Promise<void> {
     await this.createUser.execute(payload);
+  }
+
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  @ApiOperation({description: 'Login'})
+  async login(
+    @Req() req: Request & {user: User},
+  ): Promise<{accessToken: string}> {
+    return await this.generateToken.execute(req.user);
   }
 }
