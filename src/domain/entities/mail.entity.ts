7eacf15af2b3f38e3d08f4cae4e9594b95552a0d
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-01-17 14:30:27 -0300

    feat: add Mail Entity

---
+++ src/domain/entities/mail.entity.ts
@@ -0,0 +1,66 @@
+import {randomUUID} from 'node:crypto';
+import {Replace} from 'src/helpers/replace';
+
+interface MailProps {
+  title: string;
+  content: string;
+  status: MailStatus;
+  userId: string;
+  to: string;
+  sendAt: Date;
+  createdAt: Date;
+}
+
+export enum MailStatus {
+  pending = 'PENDING',
+  sent = 'SENT',
+}
+
+export class Mail {
+  private _id: string;
+  private props: Replace<MailProps, {createdAt?: Date; status?: MailStatus}>;
+
+  get id() {
+    return this._id;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get status() {
+    return this.props.status;
+  }
+
+  get userId() {
+    return this.props.userId;
+  }
+
+  get to() {
+    return this.props.to;
+  }
+
+  get sendAt() {
+    return this.props.sendAt;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public send() {
+    this.props.status = MailStatus.sent;
+  }
+
+  constructor(
+    props: Replace<MailProps, {createdAt?: Date; status?: MailStatus}>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = props;
+  }
+}
