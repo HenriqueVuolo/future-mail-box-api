c005d9a14aef8148a7d447dff82a26fabc57fbf8
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-01-19 11:04:27 -0300

    feat: add missing methods to MailsRepository

--- src/domain/repositories/mails.repository.ts
+++ src/domain/repositories/mails.repository.ts
@@ -2,4 +2,8 @@ import {Mail} from '@domain/entities/mail.entity';
 
 export abstract class MailsRepository {
   abstract create: (data: Mail) => Promise<void>;
+  abstract save: (data: Mail) => Promise<void>;
+  abstract findOne: (data: Partial<Mail>) => Promise<Mail>;
+  abstract findMany: (data: Partial<Mail>) => Promise<Mail[]>;
+  abstract delete: (id: Mail['id']) => Promise<void>;
 }
