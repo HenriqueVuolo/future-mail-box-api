3e5fd60dfb61f30234782ea8306c57e4179bee9f
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-01-19 13:53:50 -0300

    feat: add @nestjs/schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,20 @@ import {JwtStrategy} from '@infra/auth/strategies/jwt.strategy';
 import {LocalStrategy} from '@infra/auth/strategies/local.strategy';
 import {DatabaseModule} from '@infra/database/database.module';
 import {HttpModule} from '@infra/http/http.module';
+import {SendAtHandler} from '@infra/messaging/send-at.handler';
 import {Module} from '@nestjs/common';
+import {ScheduleModule} from '@nestjs/schedule';
+import {SendMail} from '@useCases/mail/send-mail';
 import {ValidateUser} from '@useCases/user/validate-user';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
-  providers: [ValidateUser, LocalStrategy, JwtStrategy],
+  imports: [DatabaseModule, HttpModule, ScheduleModule.forRoot()],
+  providers: [
+    ValidateUser,
+    SendMail,
+    LocalStrategy,
+    JwtStrategy,
+    SendAtHandler,
+  ],
 })
 export class AppModule {}
