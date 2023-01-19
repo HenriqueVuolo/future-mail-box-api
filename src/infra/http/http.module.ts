251cee389f4935d14de707e16ef978eefc1741c3
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-01-19 11:07:07 -0300

    feat: add mail use cases to HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,7 +2,11 @@ import {DatabaseModule} from '@infra/database/database.module';
 import {Module} from '@nestjs/common';
 import {JwtModule} from '@nestjs/jwt';
 import {GenerateToken} from '@useCases/auth/generate-token';
-import {CreateMail} from '@useCases/message/create-mail';
+import {CreateMail} from '@useCases/mail/create-mail';
+import {DeleteMail} from '@useCases/mail/delete-mail';
+import {FindMailsByUserId} from '@useCases/mail/find-mails-by-user-id';
+import {FindMailById} from '@useCases/mail/find-mail-by-id';
+import {UpdateMail} from '@useCases/mail/update-mail';
 import {CreateUser} from '@useCases/user/create-user';
 import {GetUser} from '@useCases/user/get-user';
 import {UpdateUserEmail} from '@useCases/user/update-user-email';
@@ -19,6 +23,16 @@ import {UserController} from './controllers/user.controller';
     }),
   ],
   controllers: [AuthController, UserController, MailController],
-  providers: [CreateMail, CreateUser, GenerateToken, GetUser, UpdateUserEmail],
+  providers: [
+    CreateMail,
+    CreateUser,
+    DeleteMail,
+    FindMailById,
+    FindMailsByUserId,
+    GenerateToken,
+    GetUser,
+    UpdateMail,
+    UpdateUserEmail,
+  ],
 })
 export class HttpModule {}
