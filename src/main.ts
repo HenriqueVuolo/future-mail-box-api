356fb11d576603a2aea4343fe02750406c315418
HenriqueSantana1 <henriquevuolo@gmail.com>
2023-03-25 09:02:44 -0300

    feat: update api port and enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {AppModule} from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('TimeBottle API')
     .setVersion('1.0')
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
